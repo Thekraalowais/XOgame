8b7d4a840441d6f49f4a72bc2936006eb0622423
thekraalowais <thekra_alowais@hotmail.com>
2018-10-14 22:04:47 +0300

    add winner function

--- main.js
+++ main.js
@@ -1,12 +1,14 @@
 $(document).ready(function () {
     console.log("XO Game");
     var player = "X";
+    var xCount = 0;
+    var oCount = 0;
     var grid = [["0", "1", "2"], ["3", "4", "5"], ["6", "7", "8"]];
     var divs = $('.grid-item');
 
     // function game() {
     divs.on('click', function () {
-        debugger;
+        // debugger;
         // Loop through rows
         if ($(this).text() === "") {
             for (var i = 0; i < 3; i++) {
@@ -29,8 +31,8 @@ $(document).ready(function () {
         } else {
             alert("Choose another cell");
         }
-
-        console.log(grid);
+        winner();
+        // console.log(grid);
         // if ($(this).text() === "") {
         //     console.log("empty");
         //     $(this).text(player);
@@ -42,7 +44,48 @@ $(document).ready(function () {
         // }
 
     });
+    function winner() {
+        //check rows
+        for (var i = 0; i < 3; i++) {
+            // console.log(grid.length); 
+            // Loop through columns
+            for (var j = 0; j < 3; j++) {
+                if (grid[i][j] === "X") {
+                    xCount++;
+                } else {
+                    oCount++
+                }
+            }
+        }
+        //check columns
+        for (var i = 0; i < 3; i++) {
+            // console.log(grid.length); 
+            // Loop through columns
+            for (var j = 0; j < 3; j++) {
+                if (grid[j][i] === "X") {
+                    xCount++;
+                } else {
+                    oCount++
+                }
+            }
+        }
+        console.log(xCount + " " + oCount);
+        // if (xCount === 3) {
+        //     alert("Congrats player X");
 
+        // } else if (oCount === 3) {
+        //     alert("Congrats player O");
+
+        // } 
+        if (grid[0][0] === "X" && grid[1][1] === "X" && grid[2][2] === "X") {
+            // alert("Congrats" + player);
+            alert("Congrats player X");
+        } else if (grid[0][2] === "X" && grid[1][1] === "X" && grid[2][0] === "X") {
+            // alert("Congrats" + player);
+            alert("Congrats player O");
+
+        }
+    }
     function switchPlayer() {
         if (player === "X") {
             player = "O";
