04946b33dc1bbf68344b7c1ba4895f80f1fa0f3d
thekraalowais <thekra_alowais@hotmail.com>
2018-10-14 23:12:31 +0300

    final logic part

--- main.js
+++ main.js
@@ -23,39 +23,42 @@ $(document).ready(function () {
                         $(this).text(player);
                         // console.log(grid[i]);
                         // console.log(grid[i][j]);
+                        winner();
                         switchPlayer();
+
                         // console.log("The array value is " + grid[i][j]);
                     }
                 }
             }
+
         } else {
             alert("Choose another cell");
         }
-        winner();
-        // console.log(grid);
-        // if ($(this).text() === "") {
-        //     console.log("empty");
-        //     $(this).text(player);
-        //     var id = $(this).attr('id');
-        //     grid[id] = player;
-        //     switchPlayer();
-        // } else {
-        //     alert("Choose another cell");
-        // }
 
     });
+
     function winner() {
         //check rows
+        // debugger;
         for (var i = 0; i < 3; i++) {
             // console.log(grid.length); 
             // Loop through columns
             for (var j = 0; j < 3; j++) {
                 if (grid[i][j] === "X") {
                     xCount++;
-                } else {
-                    oCount++
+                } else if (grid[i][j] === "O") {
+                    oCount++;
                 }
             }
+            if (xCount === 3) {
+                alert("Congrats player X");
+
+            } else if (oCount === 3) {
+                alert("Congrats player O");
+
+            }
+            xCount = 0; oCount = 0;
+
         }
         //check columns
         for (var i = 0; i < 3; i++) {
@@ -64,26 +67,26 @@ $(document).ready(function () {
             for (var j = 0; j < 3; j++) {
                 if (grid[j][i] === "X") {
                     xCount++;
-                } else {
-                    oCount++
+                } else if (grid[j][i] === "O") {
+                    oCount++;
+                }
+                if (xCount === 3) {
+                    alert("Congrats player X");
+
+                } else if (oCount === 3) {
+                    alert("Congrats player O");
                 }
             }
+            xCount = 0; oCount = 0;
+            console.log(xCount + " " + oCount);
+            // xCount = 0; oCount=0;
         }
-        console.log(xCount + " " + oCount);
-        // if (xCount === 3) {
-        //     alert("Congrats player X");
-
-        // } else if (oCount === 3) {
-        //     alert("Congrats player O");
-
-        // } 
-        if (grid[0][0] === "X" && grid[1][1] === "X" && grid[2][2] === "X") {
+        if (grid[0][0] === "X" && grid[1][1] === "X" && grid[2][2] === "X" || grid[0][0] === "O" && grid[1][1] === "O" && grid[2][2] === "O") {
             // alert("Congrats" + player);
-            alert("Congrats player X");
-        } else if (grid[0][2] === "X" && grid[1][1] === "X" && grid[2][0] === "X") {
+            alert("Congrats player " + player);
+        } else if (grid[0][2] === "X" && grid[1][1] === "X" && grid[2][0] === "X" || grid[0][2] === "O" && grid[1][1] === "O" && grid[2][0] === "O") {
             // alert("Congrats" + player);
-            alert("Congrats player O");
-
+            alert("Congrats player " + player);
         }
     }
     function switchPlayer() {
