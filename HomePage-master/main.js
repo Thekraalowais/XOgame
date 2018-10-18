544f4372ec8d4f36c5117755301e772ad3e765a9
thekraalowais <thekra_alowais@hotmail.com>
2018-10-18 06:01:21 +0300

    solve files path and add audio feature and sessionStorage feature

--- HomePage-master/main.js
+++ HomePage-master/main.js
@@ -1,88 +1,86 @@
 $(document).ready(function() {
   console.log("XO Game");
-  var player = "";
+  var player = "X";
   var xCount = 0;
   var oCount = 0;
   var gameWon = false;
   var endGame = 0;
-  var score = 0;
-  // var status = "";
   var grid = [["0", "1", "2"], ["3", "4", "5"], ["6", "7", "8"]];
   var divs = $(".grid-item");
   var playAgain = $(".playagain");
-
-  var myCat = localStorage.getItem("myCat");
-  console.log(myCat);
-
+  var reset = $(".reset");
+  var backPage = $(".backArrow");
+  var player1 = $(".player1");
+  var player2 = $(".player2");
+  //player 1
+  var currentValue =
+    sessionStorage.getItem("player1") || sessionStorage.setItem("player1", 0);
+  // if (currentValue === null) {
+  //   currentValue = sessionStorage.setItem("player1", 0);
+  // }
+  console.log(currentValue);
+  $(".player1").text(currentValue);
+  // player 2
+  var currentValue =
+    sessionStorage.getItem("player2") || sessionStorage.setItem("player2", 0);
+  // if (currentValue === null) {
+  //   currentValue = sessionStorage.setItem("player2", 0);
+  // }
+  $(".player2").text(currentValue);
   // When an input is checked,
   $(".clicked").on("click", function() {
     player = $("input[name='rbnNumber']:checked").attr("id");
     console.log(player);
-    // console.log($("input[name='rbnNumber']:checked").val());
+    board();
   });
+  function board() {
+    divs.on("click", function() {
+      // Loop through rows
+      if ($(this).text() === "") {
+        for (var i = 0; i < 3; i++) {
+          var id = $(this).attr("id");
+          // Loop through columns
+          for (var j = 0; j < 3; j++) {
+            if (grid[i][j] === id) {
+              grid[i][j] = player;
+              $(this)
+                .text(player)
+                .addClass("animated swing");
 
-  divs.on("click", function() {
-    // debugger;
-    // Loop through rows
-    if ($(this).text() === "") {
-      for (var i = 0; i < 3; i++) {
-        // console.log(grid.length);
-        var id = $(this).attr("id");
-        // console.log("The ID is " + id);
-        // Loop through columns
-        for (var j = 0; j < 3; j++) {
-          if (grid[i][j] === id) {
-            grid[i][j] = player;
-            // console.log(player);
-            // debugger;
-
-            $(this)
-              .text(player)
-              .addClass("animated swing");
-            // console.log(grid[i]);
-            // console.log(grid[i][j]);
-
-            winner();
-            localStorage.setItem("myCat", "Tom");
-            if (!gameWon) {
-              switchPlayer();
+              winner();
+              if (!gameWon) {
+                switchPlayer();
+              }
             }
-
-            // console.log("The array value is " + grid[i][j]);
           }
         }
+      } else {
+        alert("Choose another cell");
       }
-    } else {
-      alert("Choose another cell");
-    }
-  });
-
+    });
+  }
   function winner() {
     //check rows
-    // debugger;
-    // debugger;
     for (var i = 0; i < 3; i++) {
-      // console.log(grid.length);
       // Loop through columns
       for (var j = 0; j < 3; j++) {
         if (grid[i][j] === "X") {
           xCount++;
+          console.log(grid[i]);
+          console.log(grid);
         } else if (grid[i][j] === "O") {
           oCount++;
         }
       }
       if (xCount === 3) {
-        // alert("Congrats player X");
+        console.log(xCount);
         alertFunction();
-        // divs.off("click");
-        score++;
-        $(".player1").text(score);
+        divs.off("click");
+        score("player1", player1);
       } else if (oCount === 3) {
-        // alert("Congrats player O");
         alertFunction();
-        // divs.off("click");
-        score++;
-        $(".player1").text(score);
+        divs.off("click");
+        score("player2", player2);
       } else {
         endGame++;
       }
@@ -91,63 +89,51 @@ $(document).ready(function() {
     }
     //check columns
     for (var i = 0; i < 3; i++) {
-      // console.log(grid.length);
       // Loop through columns
       for (var j = 0; j < 3; j++) {
         if (grid[j][i] === "X") {
           xCount++;
+          console.log("the j is" + j + "the i is " + i);
         } else if (grid[j][i] === "O") {
           oCount++;
         }
         if (xCount === 3) {
-          //setTimeout
-          //   alert("Congrats player X");
           alertFunction();
-          // divs.off("click");
-          score++;
-          $(".player1").text(score);
+          divs.off("click");
+          score("player1", player1);
         } else if (oCount === 3) {
-          //   alert("Congrats player O");
           alertFunction();
-          // divs.off("click");
-          score++;
-          $(".player1").text(score);
+          divs.off("click");
+          score("player2", player2);
         } else {
           endGame++;
         }
       }
       xCount = 0;
       oCount = 0;
-      console.log(xCount + " " + oCount);
-      // xCount = 0; oCount=0;
+      // console.log(xCount + " " + oCount);
     }
-    if (
-      (grid[0][0] === "X" && grid[1][1] === "X" && grid[2][2] === "X") ||
-      (grid[0][0] === "O" && grid[1][1] === "O" && grid[2][2] === "O")
-    ) {
-      // alert("Congrats" + player);
-      //   alert("Congrats player " + player);
+    if (grid[0][0] === "X" && grid[1][1] === "X" && grid[2][2] === "X") {
       alertFunction();
-      // divs.off("click");
-      score++;
-      $(".player1").text(score);
-    } else if (
-      (grid[0][2] === "X" && grid[1][1] === "X" && grid[2][0] === "X") ||
-      (grid[0][2] === "O" && grid[1][1] === "O" && grid[2][0] === "O")
-    ) {
-      // alert("Congrats" + player);
-      //   alert("Congrats player " + player);
+      divs.off("click");
+      score("player1", player1);
+    } else if (grid[0][2] === "X" && grid[1][1] === "X" && grid[2][0] === "X") {
       alertFunction();
-      // divs.off("click");
-      score++;
-      $(".player1").text(score);
+      divs.off("click");
+      score("player1", player1);
+    } else if (grid[0][0] === "O" && grid[1][1] === "O" && grid[2][2] === "O") {
+      alertFunction();
+      divs.off("click");
+      score("player2", player2);
+    } else if (grid[0][2] === "O" && grid[1][1] === "O" && grid[2][0] === "O") {
+      alertFunction();
+      divs.off("click");
+      score("player2", player2);
     } else {
       endGame++;
     }
-    // console.log("the value" + endGame);
-
     if (endGame === 117) {
-      // console.log("end game");
+      document.getElementById("fail").play();
       swal("Try again");
     }
   }
@@ -161,12 +147,41 @@ $(document).ready(function() {
   function alertFunction() {
     gameWon = true;
     setTimeout(function() {
-      // alert("Congrats player " + player);
+      document.getElementById("xyz").play();
       swal("Good job!", "player " + player + " wins", "success");
     }, 50);
   }
   playAgain.on("click", function() {
     location.reload();
-    // divs.empty();
   });
+
+  reset.on("click", function() {
+    sessionStorage.clear();
+    //player 1
+    currentValue = sessionStorage.getItem("player1");
+    // if (currentValue === null) {
+    //   currentValue = sessionStorage.setItem("player1", 0);
+    // }
+    console.log(currentValue);
+    $(".player1").text(currentValue);
+    // player 2
+    currentValue = sessionStorage.getItem("player2");
+    // if (currentValue === null) {
+    //   currentValue = sessionStorage.setItem("player2", 0);
+    // }
+    $(".player2").text(currentValue);
+    // console.log("reset");
+    location.reload();
+  });
+  backPage.on("click", function() {
+    sessionStorage.clear();
+    window.open("../index.html", "_self");
+  });
+  function score(key, turn) {
+    var current = sessionStorage.getItem(key);
+    current = parseInt(current);
+    var addedVakue = current + 1;
+    sessionStorage.setItem(key, addedVakue);
+    turn.text(addedVakue);
+  }
 });
---
+++ README.md
@@ -0,0 +1,13 @@
+# XO GAME
+
+I made XO game using HTML, CSS, javaScript, (jQuery)[https://code.jquery.com/], (JavaScript library for creating particles)[https://github.com/VincentGarreau/particles.js], and (animate.css)[https://daneden.github.io/animate.css/].
+
+## Approach:
+
+The main process is to compare the Id div with the 3D array values. If they are matched then the player name will save into the array. In addition, there is a counter for each player.
+
+## Github link:
+
+## What's next:
+
+ِAdding AI and other features.
